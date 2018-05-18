fcc88e9843985ee00516e40d0a6d018c1af2c5c6
aspoka <aspoka1@gmail.com>
2018-05-18 21:07:32 +0200

    Project initialization

---
+++ desktop/src/com/aspoka1/app/office/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.aspoka1.app.office.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.aspoka1.app.office.TaskManager;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TaskManager(), config);
+	}
+}

8a9aa0dd12e442d11b535e1e1683c6f56368ad1f
aspoka <aspoka1@gmail.com>
2018-05-18 22:12:56 +0200

    Setting up

--- desktop/src/com/aspoka1/app/office/desktop/DesktopLauncher.java
+++ desktop/src/com/aspoka1/app/office/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.aspoka1.app.office.TaskManager;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "TaskManager";
+		config.width = 480;
+		config.height = 800;
+		config.resizable = false;
 		new LwjglApplication(new TaskManager(), config);
 	}
 }

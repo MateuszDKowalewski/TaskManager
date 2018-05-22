e6e072be9f5746720550a8166653014c2675a603
aspoka <aspoka1@gmail.com>
2018-05-22 20:20:42 +0200

    Add text field to AddTaskScreen. It's need make top field haigher

--- core/src/com/aspoka1/app/office/taskmanager/screens/AddTaskScreen.java
+++ core/src/com/aspoka1/app/office/taskmanager/screens/AddTaskScreen.java
@@ -1,10 +1,15 @@
 package com.aspoka1.app.office.taskmanager.screens;
 
+import java.time.LocalDate;
+
 import com.aspoka1.app.office.taskmanager.TaskManager;
+import com.aspoka1.app.office.taskmanager.ui.TextInputListinner;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.TextInputListener;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class AddTaskScreen extends AbstractScreen{
@@ -12,12 +17,31 @@ public class AddTaskScreen extends AbstractScreen{
 	private OrthographicCamera camera;
 
 	private static Sprite background = new Sprite(new Texture("screens/AddTaskScreen.png"));
+	private static BitmapFont latoFont = new BitmapFont(Gdx.files.internal("fonts/lato.fnt"));
+	private static BitmapFont lobsterFont = new BitmapFont(Gdx.files.internal("fonts/lobster.fnt"));
+	
+	private TextInputListener listener = new TextInputListinner();
+	
+	private String title;
+	private String description;
+	private String dateText;
+	private LocalDate endDate;
+	
+	private enum LastChange {NONE, TITLE, DATE, DESCRIPTION};
+	private LastChange lastChange;
 	
 	public AddTaskScreen(TaskManager app){
 		this.app = app;
 		
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, TaskManager.WIDTH, TaskManager.HEIGHT);
+		
+		title = "Tap to set title. Make it longer";
+		description = "Tap to set description. Make it longer, becouse I want to test multiline description.";
+		dateText = "22:25:2018-22:05:2018";//"Tap to set date";
+		lastChange = LastChange.NONE;
+		
+		//Gdx.input.getTextInput(listener, "Dialog Title", "Initial Textfield Value", "Hint Value");
 	}
 	
 	@Override
@@ -30,6 +54,9 @@ public class AddTaskScreen extends AbstractScreen{
 		app.batch.begin();
 
 		background.draw(app.batch);
+		lobsterFont.draw(app.batch, title, 30, 725, 420, 85, true);
+		latoFont.draw(app.batch, dateText, 30, 560 + 40, 420, 30, true);
+		latoFont.draw(app.batch, description, 30, 160 + 360, 420, 355, true);
 
 		app.batch.end();
 	}
---
+++ core/src/com/aspoka1/app/office/taskmanager/ui/TextInputListinner.java
@@ -0,0 +1,19 @@
+package com.aspoka1.app.office.taskmanager.ui;
+
+import com.badlogic.gdx.Input.TextInputListener;
+
+public class TextInputListinner implements TextInputListener{
+
+	@Override
+	public void input(String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void canceled() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
